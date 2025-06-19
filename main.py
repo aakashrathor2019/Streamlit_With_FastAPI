4566aacc3eeb26c0dc3c9c4b74397ee93b197671
Aakash Rathor <aakashrathor2019@gmail.com>
2025-06-19 18:10:48 +0530

    print statement added

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def upload_pdf(file: UploadFile = File(...), db: Session = Depends(get_db)
         db.commit()
         db.refresh(pdf_file)
         print("Susess")
-        print("remove print statement")
+        print("According to pawan sir again")
         return {"message": "PDF uploaded successfully", "file_id": pdf_file.id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
